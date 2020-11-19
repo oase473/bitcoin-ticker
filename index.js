f420cff0f6eed5f984f1c5bd94eaa3b5ff038e78
Tim Bliefert <timbliefert@Tims-Air.fritz.box>
2020-11-19 20:19:39 +0100

    erase submit butto

--- index.js
+++ index.js
@@ -13,16 +13,6 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/bitcoin", function(req, res) {
-  request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body) {
-    console.log(body);
-    var jsonData = JSON.parse(body);
-    var obj = jsonData.averages.day;
-    console.log(JSON.stringify(jsonData.display_timestamp) + " " + JSON.stringify(jsonData.display_symbol) + " " + JSON.stringify(obj));
-    res.redirect("/");
-  });
-});
-
 app.listen(3001, function() {
   console.log("Server is running...");
 });
